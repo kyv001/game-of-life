ad6429ae7937c3973e121e5971a492824ebd7662
杨韬 <3293972019@qq.com>
2020-05-05 22:16:16 +0800

    第一个完全版本

--- life_game/life_game.py
+++ life_game/life_game.py
@@ -1,71 +1,91 @@
 import pygame
 from pygame.locals import *
+from random import randint
+from copy import copy
 
-cells = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+cells = [[int(randint(0,100) < 50) for __ in range(200)] for _ in range(200)]
 
 pygame.init()
-screen = pygame.display.set_mode((160,160))
+screen = pygame.display.set_mode((800,800))
 
-def main(screen):
-    f_done = False
-    while not f_done:
+def main(screen, cells):
+    done = False
+    while not done:
         cursor = [0, 0]
         for event in pygame.event.get():
             if event.type == QUIT:
-                f_done = True
+                done = True
 
-        while cursor == [len(cells) - 1, len(cells[0]) - 1]:
+        tmp_cells = [[cells[i][j] for j in range(len(cells[i]))] for i in range(len(cells))]
+        while not cursor == [len(cells) - 1, len(cells[0]) - 1]:
             n_cells_around = 0
-            if cursor == [0, 0]:
-                if cells[cursor[0] + 1][cursor[1]]:
+            '''
+             _________________
+            |-1 -1|0  -1|+1 -1|
+            |-----|-----|-----|
+            |-1  0|0   0|+1  0|
+            |-----|-----|-----|
+            |-1 +1|0  +1|+1 +1|
+             ----------------- 
+            '''
+            try:
+                if cells[cursor[0] - 1][cursor[1] - 1]:
                     n_cells_around += 1
-                if cells[cursor[0]][cursor[1] + 1]:
-                    n_cells_around += 1
-                    
-            elif cursor[0] == 0:
-                if cells[cursor[0] + 1][cursor[1]]:
-                    n_cells_around += 1
+            except IndexError:...
+            try:
                 if cells[cursor[0]][cursor[1] - 1]:
                     n_cells_around += 1
-                if cells[cursor[0]][cursor[1] + 1]:
+            except IndexError:...
+            try:
+                if cells[cursor[0] + 1][cursor[1] - 1]:
                     n_cells_around += 1
-                    
-            elif cursor[1] == 0:
+            except IndexError:...
+            try:
                 if cells[cursor[0] - 1][cursor[1]]:
                     n_cells_around += 1
+            except IndexError:...
+            try:
                 if cells[cursor[0] + 1][cursor[1]]:
                     n_cells_around += 1
+            except IndexError:...
+            try:
+                if cells[cursor[0] - 1][cursor[1] + 1]:
+                    n_cells_around += 1
+            except IndexError:...
+            try:
                 if cells[cursor[0]][cursor[1] + 1]:
                     n_cells_around += 1
-                    
-            else:
-                if cells[cursor[0] - 1][cursor[1]]:
+            except IndexError:...
+            try:
+                if cells[cursor[0] + 1][cursor[1] + 1]:
                     n_cells_around += 1
-                if cells[cursor[0] + 1][cursor[1]]:
-                    n_cells_around += 1
-                if cells[cursor[0]][cursor[1] - 1]:
-                    n_cells_around += 1
-                if cells[cursor[0]][cursor[1] + 1]:
-                    n_cells_around += 1
+            except IndexError:...
 
-            if n_cells_around >= 3 and cells[cursor[0]][cursor[1]]:
-                cells[cursor[0]][cursor[1]] = 0
-            if n_cells_around == 2 and not cells[cursor[0]][cursor[1]]:
-                cells[cursor[0]][cursor[1]] = 1
+            if n_cells_around == 3:
+                tmp_cells[cursor[0]][cursor[1]] = 1
+            elif n_cells_around == 2:
+                pass
+            else:
+                tmp_cells[cursor[0]][cursor[1]] = 0
+
+            cursor[0] += 1
+            if cursor[0] == len(cells[0]):
+                cursor[0] = 0
+                cursor[1] += 1
+
+        cells = copy(tmp_cells)
+
+        x, y = 0, 0
+        for col in cells:
+            for cell in col:
+                pygame.draw.rect(screen,
+                                 0xFFFFFF if cell else 0x000000,
+                                 pygame.Rect((x * 4, y * 4), (4, 4)))
+                x += 1
+            x = 0
+            y += 1
 
         pygame.display.update()
+
+if __name__ == '__main__':
+    main(screen, cells)
